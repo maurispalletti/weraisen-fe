15ae0eb63905d8059af2178ce84147c038c72c30
irinarossi <irinarossi11@gmail.com>
2019-09-01 12:37:35 -0300

    cambios en pantalla login

--- src/Login.js
+++ src/Login.js
@@ -11,18 +11,18 @@ const Login = () => (
  </div>
     <form>
       {/* <form onSubmit={this.handleSignIn.bind(this)}> */}
-      <p>Inicia sesión en tu cuenta</p>
-      <p><input className="login-input" type="text" placeholder="Ingresa tu email" /></p>
-      <p><input className="login-input" type="password" placeholder="Ingresa tu contraseña" /></p>
-      <a href={'/signup'}><h6 align="left">¿Olvidaste tu contraseña?</h6></a>
+      <h3>Iniciar sesión </h3>
+      <p><input className="login-input" type="text" placeholder="Email" /></p>
+      <p><input className="login-input" type="password" placeholder="Contraseña" />
+      <a href={'/signup'}><h6 align="center">¿Olvidaste tu contraseña?</h6></a></p>
       {/* <p><input className="login-input" align="left" type="checkbox" placeholder="Recordarme"/>
         Recordarme
       </p> */}
 
-      <p><a href={'/home'} className="login-button">INICIAR SESIÓN</a></p>
+      <p><a href={'/home'} className="login-button">INGRESAR</a></p>
       <div className="signup">
-        <p>¿Primera vez en WERAISEN?</p>
-        <p><a href={'/signup'} className="signup-button">CREAR CUENTA</a></p>
+        <p>¿Primera vez en WERAISEN? <a href={'/signup'}>Registrate ya.</a></p> 
+        {/* <p><a href={'/signup'} className="signup-button">CREAR CUENTA</a></p> */}
         {/* <input type="text" ref="username" placeholder="enter you username" /> */}
         {/* <input type="password" ref="password" placeholder="enter password" /> */}
         {/* <input type="submit" value="Login" /> */}
