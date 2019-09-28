dbe285f83336b6ca9ad6a776125c47e61fbcd1ff
Mauricio Spalletti <mauricio@thirdlove.com>
2019-09-28 20:42:32 -0300

    more changes to login

--- src/Login.js
+++ src/Login.js
@@ -2,64 +2,67 @@ import React, { Component } from 'react'
 import './Login.css';
 import logo from './icons/logo.png'
 import loginServices from './services/userServices';
+import { Redirect } from 'react-router'
 
 const emailTest = 'mauricio@test.com'
 const passwordTest = 'password'
 
-const loginUser = async ({ email, password }) => {
+class Login extends Component {
+
+  state = { goToSignup: false }
+
+loginUser = async ({ email, password }) => {
+
+  console.log(`loginuser ${this.state.goToSignup}`)
+
   try {
     const response = await loginServices.login({ email, password })
 
-    console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
-    console.log(response)
-    console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
+    const { data: { id } } = response
+
+    console.log(id)
+
+    localStorage.setItem("userId", id);
+
+    // console.log(`GET ID`)
+    // localStorage.getItem("userId");
+    // console.log(`!!!!!!!!!!!` + id)
+
+    this.setState({ goToSignup: true })
 
   } catch (error) {
-    console.log(`Error using axios fetch !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
+    console.log(`Error using axios fetch`)
   }
 }
 
-class Login extends Component {
+render() {
 
-  // componentDidMount() {
-  //   axios.get('https://dog.ceo/api/breeds/image/random')
-  //     .then(response => {
-  //       console.log(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }
+  console.log(`render ${this.state.goToSignup}`)
 
-  render() {
-    return (<div className="Login">
-      <div className="Header">
-        <img src={logo} alt={"WeRaisen"} width="100" />
-        <h3 align="right">WERAISEN</h3>
-      </div>
-      <form>
-        {/* <form onSubmit={this.handleSignIn.bind(this)}> */}
-        <h3>Iniciar sesión </h3>
-        <p><input className="login-input" type="text" placeholder="Email" /></p>
-        <p><input className="login-input" type="password" placeholder="Contraseña" />
-          <input type="button" value="Login" onClick={() => loginUser({ email: emailTest, password: passwordTest })} />
-          <a className="forgotPass" href={'/signup'}><h5 align="center">¿Olvidaste tu contraseña?</h5></a></p>
-        {/* <p><input className="login-input" align="left" type="checkbox" placeholder="Recordarme"/>
-      Recordarme
-    </p> */}
-
-        <p><a href={'/home'} className="login-button">INGRESAR</a></p>
-        <div className="signup">
-          <h5>¿Primera vez en WERAISEN? <a className="forgotPass" href={'/signup'}>Registrate ya.</a></h5>
-          {/* <p><a href={'/signup'} className="signup-button">CREAR CUENTA</a></p> */}
-          {/* <input type="text" ref="username" placeholder="enter you username" /> */}
-          {/* <input type="password" ref="password" placeholder="enter password" /> */}
-          {/* <input type="submit" value="Login" /> */}
-        </div>
-      </form>
-    </div>)
-
+  if (this.state.goToSignup) {
+    return <Redirect to="/signup" />
   }
+
+  return (<div className="Login">
+    <div className="Header">
+      <img src={logo} alt={"WeRaisen"} width="100" />
+      <h3 align="right">WERAISEN</h3>
+    </div>
+    <form>
+      {/* <form onSubmit={this.handleSignIn.bind(this)}> */}
+      <h3>Iniciar sesión </h3>
+      <p><input className="login-input" type="text" placeholder="Email" /></p>
+      <p><input className="login-input" type="password" placeholder="Contraseña" />
+        <input type="button" value="Login" onClick={() => this.loginUser({ email: emailTest, password: passwordTest })} />
+        <a className="forgotPass" href={'/signup'}><h5 align="center">¿Olvidaste tu contraseña?</h5></a></p>
+      <p><a href={'/home'} className="login-button">INGRESAR</a></p>
+      <div className="signup">
+        <h5>¿Primera vez en WERAISEN? <a className="forgotPass" href={'/signup'}>Registrate ya.</a></h5>
+      </div>
+    </form>
+  </div>)
+
+}
 };
 
 export default Login;
