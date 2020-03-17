ceec9ad032628ac3ef56cc75ccc9ec1473ac61e4
Alvo <alvoscares@gmail.com>
2020-03-17 16:12:28 -0300

    Se agregaron los botones para sibirfotos en el signup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import $ from 'jquery'; 
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './bootstrap.min.css'
 
