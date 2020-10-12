5cffa477eac128378e969761d423691a0b1eb729
Mauricio Spalletti <spalletti.mauricio@gmail.com>
2020-10-11 22:15:39 -0300

    add new warning profile

--- src/Profile.js
+++ src/Profile.js
@@ -24,6 +24,8 @@ class Profile extends Component {
     isGuide: false,
     knowledge: [],
     updateOk: false,
+    showWarning: false,
+    activeMatches: 0,
   }
 
   // AGREGAR BOTON DE EDIT PARA PODER EDITAR INFO 
@@ -67,7 +69,7 @@ class Profile extends Component {
         })
         const { data: { id } } = response
         console.log(id);
-        this.setState({ editable: false, updateOk:true })
+        this.setState({ editable: false, updateOk: true })
       } else {
         this.setState({ notLoggedInUser: true })
       }
@@ -87,8 +89,6 @@ class Profile extends Component {
   }
 
   async componentDidMount() {
-
-    
     const {
       firstName,
       lastName,
@@ -114,7 +114,7 @@ class Profile extends Component {
       console.log('IS GUIDEEEE')
       isGuide = true;
     }
-    
+
     const initialValues = {
       firstName,
       lastName,
@@ -129,25 +129,56 @@ class Profile extends Component {
     this.setState({ initialValues, isActiveGuide, knowledge, isGuide })
   }
 
-  estadoGuia =  async () => {
-    this.setState({ isActiveGuide: !this.state.isActiveGuide });   
+  estadoGuia = async () => {
     try {
-      
+      let shouldShowWarning = false;
+      let activeMatchesToShow = 0;
+
       //cuactualizo si el ususario esta activo como guia o no
       const userId = localStorage.getItem("userId");
       if (userId) {
         await userServices.updateGuiaActivo({
           userId: userId,
           isActiveGuide: !this.state.isActiveGuide
-        })}
-    
-  } catch (error) {
-    console.log(error)
-    console.error(`There was an error trying to update guide status`)
-  }    
+        })
+
+        if (this.state.isActiveGuide) {
+          //count if guide has active matches
+          const { data } = await userServices.getActiveMatchesByGuide(userId);
+          if (data && data.length > 0) {
+            activeMatchesToShow = data.length;
+            shouldShowWarning = true;
+          }
+        }
+
+        this.setState({ isActiveGuide: !this.state.isActiveGuide, showWarning: shouldShowWarning, activeMatches: activeMatchesToShow });
+      }
+    } catch (error) {
+      console.log(error)
+      console.error(`There was an error trying to update guide status`)
+    }
   }
 
- 
+  renderWarning() {
+    if (this.state.showWarning && this.state.activeMatches > 0) {
+      return (
+        <div class="alert alert-dismissible alert-secondary"
+          style={{ background: '#d48e4b', maxWidth: '300px', textAlign: 'center', marginLeft: 'auto', marginRight: ' auto' }} role="alert">
+          <strong>Te desactivaste como guía y no volverás a aparecer en búsquedas futuras hasta que te actives otra vez. Recordá que aún tenés {this.state.activeMatches}
+            {this.state.activeMatches > 1 ? ' encuentros ya organizados ' : ' encuentro ya organizado '} para los próximos días.
+          Si deseas reprogramar o cancelar {this.state.activeMatches > 1 ? 'esos encuentros, ponete en contacto con tus turistas ' : 'ese encuentro, ponete en contacto con tu turista '}
+          para decidir como proseguir.</strong>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      )
+    } else {
+      return <div></div>
+    }
+  }
+
+
   render() {
     if (this.state.goToHome) {
       return <Redirect to="/home" />
@@ -160,9 +191,10 @@ class Profile extends Component {
     }
 
     if (this.state.initialValues) {
+      const { showWarning, activeMatches } = this.state;
       return (
         <div className="Profile" >
-     <Header></Header>
+          <Header></Header>
           <div className="BodyProfile" style={{ marginTop: '15px', marginBottom: '15px' }}>
             <Formik
               // setear initial values con el did mount o will mount llamando al get
@@ -175,10 +207,10 @@ class Profile extends Component {
                   <h2>¡Hola, {this.state.initialValues.firstName}!</h2>
 
                   <div>
-							      <div className="FotoPerfil">
-								      <img src= {this.state.initialValues.profilePicture} alt="profile" style={{ width: '200px', height: '200px', objectFit: 'cover', borderTopRightRadius: '12px', borderTopLeftRadius: '12px', borderBottomLeftRadius: '12px', borderBottomRightRadius: '12px' }} />
-							     </div>
-					      	</div>
+                    <div className="FotoPerfil">
+                      <img src={this.state.initialValues.profilePicture} alt="profile" style={{ width: '200px', height: '200px', objectFit: 'cover', borderTopRightRadius: '12px', borderTopLeftRadius: '12px', borderBottomLeftRadius: '12px', borderBottomRightRadius: '12px' }} />
+                    </div>
+                  </div>
 
                   <div className="title">
                     <FieldWithError disabled={!this.state.editable} name="firstName" placeholder="Nombre" aria-label="name" className="input" />
@@ -189,15 +221,10 @@ class Profile extends Component {
                     Apellido
                   </div>
                   <div className="title">
-                    <FieldWithError disabled={true} name="birthDate" placeholder="Fecha de Nacimiento" aria-label="birthDate" className="input"/>
+                    <FieldWithError disabled={true} name="birthDate" placeholder="Fecha de Nacimiento" aria-label="birthDate" className="input" />
                     Fecha de nacimiento
                   </div>
-                
-                  {/* <div className="title">
-                  <DropdownGender disabled={!this.state.editable} name="gender" styleName={"input"} options={genders} />
-                  Género
-                  </div> */}
-               
+
                   <div className="title">
                     <FieldWithError disabled={true} name="email" placeholder="Email" aria-label="email" className="input" />
                   Email
@@ -205,48 +232,32 @@ class Profile extends Component {
 
                 </div>
                 <br></br>
-                <a href="/MyReviews" className="lead" style={{cursor:'pointer', fontSize:' 16px'}} onClick={() => this.setState({ goToMyReviews: true })}>Ver valoraciones e informes</a>
-              
+                <a href="/MyReviews" className="lead" style={{ cursor: 'pointer', fontSize: ' 16px' }} onClick={() => this.setState({ goToMyReviews: true })}>Ver valoraciones e informes</a>
+
 
                 <div className="guideSection">
-                  {this.state.knowledge && this.state.knowledge.length > 0 } 
+                  {this.state.knowledge && this.state.knowledge.length > 0}
                   <div className="be-guide" style={{ display: this.state.isGuide ? 'block' : 'none' }}>
-                   Mostrarme activo 
+                    Mostrarme activo
                       <label class="switch">
                       <input type="checkbox" checked={this.state.isActiveGuide} onClick={() => this.estadoGuia()} />
                       <span class="slider round"></span>
                     </label>
                   </div>
+
+                  {this.renderWarning()}
                 </div>
-  
-              
+
 
                 <div className="buttonsSectionGuia">
-                  <input type="button" className="btn-primero" style={{width: '257px'}}
+                  <input type="button" className="btn-primero" style={{ width: '257px' }}
                     value={this.state.isGuide ? "Actualizar mis datos de guía" : "Quiero ser guía"}
                     onClick={() => this.setState({ goToGuideProfile: true })} />
                 </div>
-            
-                {/* <div class="btn-group" role="group">
-                  <input type="button" style={{marginRight: '10px'}} className="btn-tercero" value={this.state.editable ? "Cancelar" : "Editar mis datos"}
-                    onClick={() => this.toggleEditInfo()} />
-                    <br></br>
-                  <input type="submit" className="btn-tercero" value="Guardar" disabled={!this.state.editable}  />
-                  
+
+                <div className="cerrarSesion">
+                  <Buttom link={'/login'} className={"botons"} name={"Cerrar sesión"} />
                 </div>
-                {this.state.updateOk && (
-                   <p className="updateOk" >
-                     ¡Tus datos se guardaron!
-                   </p>
-                 )} */}
-
-
-                  {/* <input type="button" className="cerrarSesion" value="Cerrar sesión"
-                    onClick={() => this.setState({ goToGuideProfile: true })} /> */}
-
-                  <div className="cerrarSesion">
-                    <Buttom link={'/login'} className={"botons"} name={"Cerrar sesión"} />
-                  </div>
 
 
                 {this.state.notLoggedInUser && (
